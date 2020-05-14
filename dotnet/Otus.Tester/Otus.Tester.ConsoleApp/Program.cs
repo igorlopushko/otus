90dab0fd00eba188dac32049d1daaae45ae4ca83
Igor Lopushko <lopushko.igor@gmail.com>
2020-05-14 07:46:42 +0300

    Rename folders

--- dotnet/Otus.Tester/Otus.Tester.ConsoleApp/Program.cs
+++ dotnet/Otus.Tester/Otus.Tester.ConsoleApp/Program.cs
@@ -9,10 +9,10 @@ namespace Otus.Tester.ConsoleApp
         static void Main(string[] args)
         {
             ITask task = new StringLengthTask();
-            string path = $"data/0.String/";
+            string path = $"data/00.String/";
 
             //ITask task = new LuckyTicketsTask();
-            //string path = $"data/1.Tickets/";
+            //string path = $"data/01.Tickets/";
 
             var t = new TestRunner(task, path);
             t.Run();
