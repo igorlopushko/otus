57e6a150a45c82fceac79b3d195cc4f5145ebed0
Igor Lopushko <lopushko.igor@gmail.com>
2020-09-26 11:47:58 +0300

    Add implementation of the Tarjan algorithm for tolopogic sort.

--- dotnet/10.graph-search-and-sort/Otus.Tester.ConsoleApp/Program.cs
+++ dotnet/10.graph-search-and-sort/Otus.Tester.ConsoleApp/Program.cs
@@ -10,17 +10,23 @@ namespace Otus.Tester.ConsoleApp
         {
             var result = PrintMenu();
             ITask task;
+            var path = "";
 
             switch (result)
             {
                 case 1:
                     task = new TopologicalSortDemukronTask();
+                    path = "data/01.Topological Sort/";
+                    break;
+                case 2:
+                    task = new TopologicalSortTarjanTask();
+                    path = "data/02.Topological Sort/";
                     break;
                 default:
                     return;
             }
 
-            var t = new TestRunner(task, "data/01.Topological Sort/");
+            var t = new TestRunner(task, path);
             t.Run();
 
             Console.WriteLine("Completed. Press ENTER to exit.");
@@ -35,6 +41,8 @@ namespace Otus.Tester.ConsoleApp
                 Console.WriteLine("Выберите номер задания/решения:");
                 Console.WriteLine("{0} - Топологическая сортировка Алгоритм Демукрона",
                     1.ToString().PadRight(rightPadding));
+                Console.WriteLine("{0} - Топологическая сортировка Алгоритм Тарьяна",
+                    2.ToString().PadRight(rightPadding));
 
                 int result;
                 var answer = Console.ReadLine();
---
+++ dotnet/10.graph-search-and-sort/Otus.Tester.ConsoleApp/Tasks/TopologicalSortTarjanTask.cs
@@ -0,0 +1,110 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Otus.Tester.ConsoleApp.Base;
+
+namespace Otus.Tester.ConsoleApp.Tasks
+{
+    public class TopologicalSortTarjanTask : ITask
+    {
+        public string[] Run(string[] data)
+        {
+            var matrix = new int[data.Length, data.Length];
+            
+            for (var i = 0; i < data.Length; i++)
+            {
+                var values = data[i].Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                var valuesCount = values.Length > 1 ? 1 : 0;
+
+                for (var j = 0; j < data.Length; j++)
+                    if (values.Length > 1 &&
+                        valuesCount < values.Length &&
+                        int.Parse(values[valuesCount]) - 1 == j)
+                    {
+                        matrix[i, j] = 1;
+                        valuesCount++;
+                    }
+                    else
+                    {
+                        matrix[i, j] = 0;
+                    }
+            }
+
+            var result = Tarjan(matrix);
+            var stringResult = new List<string>();
+
+            var str = new StringBuilder();
+            
+            for (var i = 0; i < result.Length; i++)
+            {
+                str.Append(result[i]);
+                if (i < result.Length - 1)
+                {
+                    str.Append(" ");
+                }
+                
+            }
+            stringResult.Add(str.ToString());
+            
+            return stringResult.ToArray();
+        }
+
+        private int[] Tarjan(int[,] adjacencyMatrix)
+        {
+            var stack = new Stack<int>();
+            var visited = new HashSet<int>();
+
+            while (visited.Count < adjacencyMatrix.GetLength(0))
+            {
+                for (var vertix = 0; vertix < adjacencyMatrix.GetLength(0); vertix++)
+                {
+                    Dfs(vertix, adjacencyMatrix, stack, visited);
+                }
+            }
+
+            var result = new int[stack.Count];
+            for (var i = 0; i < result.Length; i++)
+            {
+                result[i] = stack.Pop() + 1;
+            }
+
+            return result;
+        }
+
+        private void Dfs(int vertix, int[,] adjacencyMatrix, Stack<int> stack, HashSet<int> visited)
+        {
+            if (visited.Contains(vertix)) return;
+            
+            // get child vertices
+            var childVertices = new List<int>();
+            for (var col = 0; col < adjacencyMatrix.GetLength(1); col++)
+            {
+                if (adjacencyMatrix[vertix, col] == 1 && !visited.Contains(adjacencyMatrix[vertix, col]))
+                {
+                    childVertices.Add(col);
+                }
+            }
+
+            // if no child vertices stop and add to stack
+            if (childVertices.Count == 0)
+            {
+                stack.Push(vertix);
+            }
+
+            // go down to children
+            foreach (var child in childVertices)
+            {
+                Dfs(child, adjacencyMatrix, stack, visited);
+            }
+
+            // mark as visited
+            visited.Add(vertix);
+            
+            // add to the result if not already added
+            if (!stack.Contains(vertix))
+            {
+                stack.Push(vertix);
+            }
+        }
+    }
+}
