68ea96b445bc864ed1749be2cf31f7da324a17e4
Igor Lopushko <lopushko.igor@gmail.com>
2020-10-11 23:01:14 +0300

    Add implementation for logical problem solving tasks

---
+++ dotnet/14.problem-solving/Otus.ProblemSolving.Task1/Program.cs
@@ -0,0 +1,86 @@
+﻿/*
+Суслик и Хома нашли кладовку с горохом и решили его поделить.
+
+— Ты чего мой горох берёшь!
+— Как твой?
+— Подо мной, значит мой!
+— Ах так? Тогда давай делить!
+— Давай!
+
+
+Суслик набрал себе a/b гороха, а Хома c/d гороха от общего количества.
+
+Найдите дробь, которая покажет, какую часть
+от общего количества гороха они себе набрали.
+
+Ответ записать в виде несократимой дроби.
+Значение каждой дроби меньше единицы,
+сумма дробей не больше единицы.
+
+Начальные данные: одна строка в виде a/b+c/d.
+Строка записана именно в таком формате, вместо букв записаны числа, каждое число от 1 до 10000.
+Вывод результата: ответ в виде x/y, представляющих собой несократимую дробь.
+Максимальное значение дроби 1/1.
+
+ x     a     c
+
+--- = --- + ---
+
+ y     b     d
+  
+ */
+
+using System;
+
+namespace Otus.ProblemSolving.Task1
+{
+    class Program
+    {
+        static int GCD(int a, int b)
+        {
+            if (a == b) return a;
+            
+            if (a == 0) return b;
+            
+            if (b == 0) return a;
+            
+            if (IsEven(a) && IsEven(b)) return GCD(a >> 1, b >> 1) * 2;
+            
+            if (IsEven(a) && IsOdd(b)) return GCD(a >> 1, b);
+            
+            if (IsOdd(a) && IsEven(b)) return GCD(a, b >> 1);
+            
+            if (a > b) return GCD((a - b) >> 1, b);
+            return GCD(a, (b - a) >> 1);
+        }
+
+        static bool IsEven(int number)
+        {
+            return (number & 1) == 0;
+        }
+        
+        static bool IsOdd(int number)
+        {
+            return (number & 1) == 1;
+        }
+        
+        static void Main(string[] args)
+        {
+            string[] line = Console.ReadLine().Split('+', '/');
+
+            var a = int.Parse(line[0]);
+            var b = int.Parse(line[1]);
+            var c = int.Parse(line[2]);
+            var d = int.Parse(line[3]);
+
+            int x = a * d + b * c;
+            int y = b * d;
+
+            int gcd = GCD(x, y);
+            x /= gcd;
+            y /= gcd;
+            
+            Console.WriteLine(x + "/" + y);
+        }
+    }
+}
---
+++ dotnet/14.problem-solving/Otus.ProblemSolving.Task4/Program.cs
@@ -0,0 +1,69 @@
+﻿/*
+
+Каждый элемент квадратной матрицы размеренности N x N равен нулю, либо единице. Найдите количество «островов», образованных единицами. 
+Под «островом» понимается группа единиц (либо одна единица), со всех сторон окруженная нулями (или краями матрицы). 
+Единицы относятся к одному «острову», если из одной из них можно перейти к другой «наступая» на единицы, расположенные в соседних клетках. 
+Соседними являются клетки, граничащие по горизонтали или вертикали.
+
+Входные данные
+В первой строке вводится натуральное число N не больше 100 - размер квадратной матрицы.
+В следующих N строках задаются элементы матрицы, по N чисел через пробел на каждой.
+
+Выходные данные
+Вывести единственное число - количество островов.
+ 
+ */
+using System;
+
+namespace Otus.ProblemSolving.Task4
+{
+    class Program
+    {
+        private static int[,] matrix;
+        private static int n;
+        
+        static void Main()
+        {
+            n = int.Parse(Console.ReadLine());
+
+            matrix = new int[n, n];
+
+            for (int x = 0; x < matrix.GetLength(0); x++)
+            {
+                string[] values = Console.ReadLine().Trim().Split(' ');
+                
+                for (int y = 0; y < values.Length; y++)
+                {
+                    matrix[x, y] = int.Parse(values[y]);
+                }
+            }
+
+            int islands = 0;
+            for (int x = 0; x < matrix.GetLength(0); x++)
+            {
+                for (int y = 0; y < matrix.GetLength(1); y++)
+                {
+                    if (matrix[x, y] == 1)
+                    {
+                        islands++;
+                        Go(x, y);
+                    }
+                }
+            }
+            
+            Console.WriteLine(islands);
+        }
+
+        static void Go(int x,  int y)
+        {
+            if (x < 0 || x >= n) return;
+            if (y < 0 || y >= n) return;
+            if (matrix[x, y] == 0) return;
+            matrix[x, y] = 0;
+            Go(x - 1, y);
+            Go(x + 1, y);
+            Go(x, y - 1);
+            Go(x , y + 1);
+        }
+    }
+}
